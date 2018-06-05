09dbf12c83d578d2035d4f23b8d0d6a574eee2e9
michael <michael.gallipo@gmail.com>
2018-06-04 21:46:18 -0400

    Added redirects

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -26,7 +26,7 @@ class Client::ProductsController < ApplicationController
       color: params["color"]
     }
     response = Unirest.post("http://localhost:3000/api/products", parameters: product_params).body
-    render "create.html.erb"
+    redirect_to "/client/products/#{response['id']}"
   end
 
   def edit
@@ -44,13 +44,13 @@ class Client::ProductsController < ApplicationController
       color: params["color"]
     }
     response = Unirest.patch("http://localhost:3000/api/products/#{params['id']}", parameters: product_params).body
-    render "update.html.erb"
+    redirect_to "/client/products/#{response['id']}"
   end    
 
   def destroy
     product_id = params[:id]
     response = Unirest.delete("http://localhost:3000/api/products/#{product_id}")
-    render "destroy.html.erb"
+    redirect_to "/client/products"
   end
 
 
