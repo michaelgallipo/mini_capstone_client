9392873bd362f7128d7c46a36e65a40961379446
michael <michael.gallipo@gmail.com>
2018-06-05 01:32:00 -0400

    added sales tax calculation and discount message plus formatting

--- app/controllers/client/products_controller.rb
+++ app/controllers/client/products_controller.rb
@@ -26,6 +26,7 @@ class Client::ProductsController < ApplicationController
       color: params["color"]
     }
     response = Unirest.post("http://localhost:3000/api/products", parameters: product_params).body
+    flash[:message] = "Product sucessfully created"
     redirect_to "/client/products/#{response['id']}"
   end
 
@@ -44,12 +45,14 @@ class Client::ProductsController < ApplicationController
       color: params["color"]
     }
     response = Unirest.patch("http://localhost:3000/api/products/#{params['id']}", parameters: product_params).body
+    flash[:message] = "Proudct successfully updated"
     redirect_to "/client/products/#{response['id']}"
   end    
 
   def destroy
     product_id = params[:id]
     response = Unirest.delete("http://localhost:3000/api/products/#{product_id}")
+    flash[:message] = "Product successfully deleted"
     redirect_to "/client/products"
   end
 
